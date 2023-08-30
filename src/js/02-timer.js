d585fa9739129da6df5fe09dd56393367887a71b
KarolMalinowski0605 <126602802+KarolMalinowski0605@users.noreply.github.com>
2023-08-30 16:29:57 +0200

    correct 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,10 +19,10 @@ const secondsValue = document.querySelector('[data-seconds]');
 
 
 const options = {
-  enableTime: true,     //  wyboru godziny
-  time_24hr: true,      //  trybu 24h
-  defaultDate: new Date(),   //  aktualna data i czas
-  minuteIncrement: 1,   // interwał wyboru minut
+  enableTime: true,     
+  time_24hr: true,      
+  defaultDate: new Date(),   
+  minuteIncrement: 1,   
   onClose(selectedDates) {
     
     const selectedDate = selectedDates[0];
@@ -42,36 +42,28 @@ const options = {
 flatpickr(dateTimePicker, options);
 
 
-
-
-
-// Nasłuchiwanie kliknięcia na przycisk "Start"
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
 
-  // Parsowanie wybranej daty z pola wyboru
   const selectedDate = flatpickr.parseDate(dateTimePicker.value);
   const currentDate = new Date();
-
-  // Obliczenie różnicy czasu między aktualną datą a wybraną datą
   const timeDifference = selectedDate - currentDate;
 
-  // Sprawdzenie, czy różnica czasu jest większa od zera (wybrana data jest w przyszłości)
   if (timeDifference <= 0) {
     Notiflix.Notify.failure("Please choose a date in the future", {
       timeout: 5000, 
     });
     startButton.disabled = false;
-    return;
+  return;
   }
 
   let intervalId;
-  // Uruchomienie interwału odliczającego czas
+ 
   intervalId = setInterval(() => {
     const currentTime = new Date();
     const timeLeft = selectedDate - currentTime;
 
-    // Sprawdzenie, czy pozostały czas jest mniejszy lub równy zeru
+   
     if (timeLeft <= 0) {
       clearInterval(intervalId);
       updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
@@ -83,7 +75,17 @@ startButton.addEventListener('click', () => {
   }, 1000);
 });
 
-// Funkcja do konwersji milisekund na obiekt czasu {dni, godziny, minuty, sekundy}
+function updateTimerDisplay(time) {
+  daysValue.textContent = addLeadingZero(time.days);
+  hoursValue.textContent = addLeadingZero(time.hours);
+  minutesValue.textContent = addLeadingZero(time.minutes);
+  secondsValue.textContent = addLeadingZero(time.seconds);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -98,18 +100,9 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-// Funkcja dodająca wiodący zero do wartości mniejszych niż 10
-function addLeadingZero(value) {
-  return value.toString().padStart(2, '0');
-}
 
-// Funkcja aktualizująca wyświetlanie licznika
-// function updateTimerDisplay(time) {
-//   daysValue.textContent = addLeadingZero(time.days);
-//   hoursValue.textContent = addLeadingZero(time.hours);
-//   minutesValue.textContent = addLeadingZero(time.minutes);
-//   secondsValue.textContent = addLeadingZero(time.seconds);
-// }
+
+
 
 
 
